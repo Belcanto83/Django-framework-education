dd1dc6a6f4bf548905dead19ca5d6df5e01f3a3a
Aleksey <farego@yandex.ru>
2018-10-21 17:48:55 +0300

    Using of placeholders

--- server/mainapp/views.py
+++ server/mainapp/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
+from django.template import Template, Context
+from django.template.loader import get_template, render_to_string
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    # template = Template(
+    #     'Hello {{ name }} !'
+    # )
+    template = get_template('mainapp/index.html')
+    # context = Context({
+    #     'title': 'Привет. Украшения ручной работы Silver Wind'
+    # })
+    context = {
+        'title': 'Украшения ручной работы Silver Wind'
+    }
+    response_string = template.render(context)
+    return HttpResponse(response_string)
+    # return render(request, 'mainapp/index.html')
 
 
 def contacts(request):
-    return render(request, 'mainapp/Contacts.html')
+    context = {
+        'phone': '+7 495 555 55 55',
+        'address': 'г. Москва, Игарский проезд, 12',
+        'mail': 'gifts@mail.ru'
+    }
+    response_string = render_to_string(
+        'mainapp/Contacts.html',
+        context
+    )
+    return HttpResponse(response_string)
+    # return render(request, 'mainapp/Contacts.html')
 
 
 def catalogue(request):
