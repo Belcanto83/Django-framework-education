29eeec85861f2578c7f9cb4738f77b0574569599
Aleksey <farego@yandex.ru>
2018-10-26 07:46:08 +0300

    Lesson 3. Preliminary

--- server/products/urls.py
+++ server/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from products.views import products_list, products_detail
 
+app_name = 'products'
 
 urlpatterns = [
-    path('', products_list),
-    path('<int:idx>', products_detail)
+    path('', products_list, name='list'),
+    path('<int:idx>/', products_detail, name='detail')
 ]
--- server/products/views.py
+++ server/products/views.py
@@ -12,7 +12,6 @@ def products_list(request):
     return render(request, 'products/Catalogue.html', context)
 
 
-#TODO Get only one product
 def products_detail(request, idx):
     context = {}
     with open('products/data/products.json', 'r', encoding='UTF-8') as file:
